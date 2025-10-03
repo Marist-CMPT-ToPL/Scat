9821db0b3022c8974680f38184fba5fc72505312
DavidMatuteJimenez <david.matutejimenez1@marist.edu>
2025-10-02 20:50:16 -0400

    added GenerateAst from crafting interpreters

--- generateAst.java
+++ generateAst.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+
 public class generateAst {
-    
+    public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      System.err.println("Usage: generate_ast <output directory>");
+      System.exit(64);
+    }
+    String outputDir = args[0];
+  }
 }
