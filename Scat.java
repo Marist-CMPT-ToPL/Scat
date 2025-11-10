e023194661b84936bd3a5aaf7cc629f9697e95f3
DavidMatuteJimenez <david.matutejimenez1@marist.edu>
2025-11-09 19:28:14 -0500

    Scat updated with the resolver and interpreter

--- Scat.java
+++ Scat.java
@@ -7,9 +7,14 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+//import java.util.Scanner;
+
+//import javax.naming.spi.Resolver;
 
 public class Scat {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    private static final Interpreter interpreter = new Interpreter();
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +32,7 @@ public class Scat {
         run(new String(bytes, Charset.defaultCharset()));
         
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -46,14 +52,16 @@ public class Scat {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
+
         Parser parser = new Parser(tokens);
         List<Stmt> statements = parser.parse();
 
         if (hadError) return;
 
-        for (Stmt statement : statements) {
-            System.out.println(new AstPrinter().print(statement));
-        }
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
@@ -71,5 +79,10 @@ public class Scat {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+            "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
