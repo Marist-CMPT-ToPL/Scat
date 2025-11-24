da66d3da639e0b14cfb5f09d44c014127b350618
DavidMatuteJimenez <david.matutejimenez1@marist.edu>
2025-11-24 17:04:15 -0500

    updated token types

--- TokenType.java
+++ TokenType.java
@@ -15,7 +15,9 @@ enum TokenType {
     IDENTIFIER, STRING, NUMBER,
 
     // Keywords.
-    SCAT, WHILE, IF, FOR, VAR, ZIP, OR, AND, FUNCTION, CLASS, ELSE, TRUE, FALSE, RETURN, EOF, THIS, SUPER
+    SCAT, WHILE, IF, FOR, VAR, ZIP, OR, AND, FUNCTION,
+    CLASS, ELSE, TRUE, FALSE, RETURN, GRAB, REPLACE, 
+    COMBINE, EOF, THIS, SUPER
 }
 
 //Note! Might have to change because our set of keywords is different?
